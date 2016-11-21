9d367e25669061797b645dfac811d79f532cbb14
Wesley Smith <wesleyfsmith@gmail.com>
2016-11-21 12:33:35 -0700

    Initial commit

---
+++ schema-fields.js
@@ -0,0 +1,45 @@
+export const getFields = (schema, isObject) => {
+  schema= schema._schema;
+
+  //TODO:
+
+  let obj = {};
+  let arr = [];
+
+  _.each(schema, function(value, key) {
+
+    //skip if key is part of sub Array
+    //TODO: check if parent has specific rules for children on sub schema
+    if (key.includes('$') || key.includes('.') ) {
+      return;
+    }
+
+    if (isObject) {
+      obj[key] = 1;
+    } else {
+      arr.push(key);
+    }
+  });
+
+  if (isObject) {
+    return obj;
+  } else {
+    return arr;
+  }
+}
+
+Meteor.Collection.prototype.getFields = function() {
+  return getFields(this.simpleSchema(), false);
+}
+
+Meteor.Collection.prototype.getFieldsAsObject = function() {
+  return getFields(this.simpleSchema(), true);
+}
+
+SimpleSchema.prototype.getFields = function() {
+  return getFields(this, false);
+}
+
+SimpleSchema.prototype.getFieldsAsObject = function() {
+  return getFields(this, true);
+}
