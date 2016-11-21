4c0a6bc70f3641f713a4a44510bbe386b6ccd928
Wesley Smith <wesleyfsmith@gmail.com>
2016-11-21 12:42:26 -0700

    Added version constraint

--- package.js
+++ package.js
@@ -14,6 +14,6 @@ Package.onUse(function(api) {
   api.versionsFrom('1.3');
   api.use('ecmascript');
   api.use('mongo');
-  api.use('aldeed:simple-schema');
+  api.use('aldeed:simple-schema>=1.5.3');
   api.mainModule('schema-fields.js');
 });
